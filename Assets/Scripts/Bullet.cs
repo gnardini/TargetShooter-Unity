0038f75c6d4653d8e32a0c5f12cd859d68eabfc4
gnardini <gonzalo.nardini.dev@gmail.com>
2016-07-23 15:15:24 -0300

    First working prototype

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.z > 200) {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < 2.8f) {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter (Collision col) {
+        if(col.gameObject.name == "Bullet") {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
